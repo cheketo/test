fe758b1b5e54fdb3da32ab680881ac327076f7cf
Alejandro Romero <romero.m.alejandro@gmail.com>
2016-02-05 12:52:57 -0300

    Checking new changes

--- welcome.php
+++ welcome.php
@@ -1,10 +1,9 @@
-//this is a PHP test
 <?php 
-
+//this is a PHP test
 function welcomeToMac($Name){
-	echo $Name.", this is what i'm talking about...";
+	echo $Name.", this is what i'm talking about!";
 }
 
-welcomeToMac();
+welcomeToMac('Hey');
 
 ?>
